8d454ed2a5e09863d841d77bdf50767a88e0e13f
Tami Reichold <reichold.tamara@gmail.com>
2019-09-11 11:55:01 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# Your Code Here
+def map(dune)
+  dune.map {|e| e*2}
+end
+
+def generalized_reduce(source_array, starting value=10)
+  source_array.reduce(starting_value) {|sum, n| sum + n}
+end
+
+  
