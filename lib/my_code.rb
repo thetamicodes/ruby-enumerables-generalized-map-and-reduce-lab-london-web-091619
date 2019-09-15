dba8800a351fe03cc468c3390d24cdbb9667e70a
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 15:33:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,9 +2,16 @@ def map(array, &block)
  array.map(&block)
 end
 
-def generalized_reduce(source_array, starting_point = 6, &block)
+def reduce(source_array, starting_point = 6, &block)
  if starting_point.nil? source_array.reduce(&block) 
   else source_array.reduce(starting_point, &block)
  end
 end
 
+# def reduce(array, start_point = nil, &block)
+# if start_point.nil?
+#   array.reduce(&block)
+# else
+#   array.reduce(start_point, &block)
+# end
+# end
