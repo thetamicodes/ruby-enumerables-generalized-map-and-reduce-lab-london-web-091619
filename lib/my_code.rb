5235b30dbedf7e08eb4508f91b1e2c09911a9692
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 15:34:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,8 +3,10 @@ def map(array, &block)
 end
 
 def reduce(source_array, starting_point = 6, &block)
- if starting_point.nil? source_array.reduce(&block) 
-  else source_array.reduce(starting_point, &block)
+ if starting_point.nil? 
+   source_array.reduce(&block) 
+  else 
+    source_array.reduce(starting_point, &block)
  end
 end
 
