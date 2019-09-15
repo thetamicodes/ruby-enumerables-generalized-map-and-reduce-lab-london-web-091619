7679e77e6fe44cdab65a95c73d9078b626c7572d
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 15:25:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,6 +5,5 @@ end
 def generalized_reduce(source_array, starting_value, &block)
  if starting_value.nil? source_array.reduce(&block) 
   else source_array.reduce(starting_value, &block)
- end
 end
 
