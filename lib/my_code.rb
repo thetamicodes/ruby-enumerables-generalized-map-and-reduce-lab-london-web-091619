473159de19e9e39b10287b40d95996da03331d2e
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 15:26:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map(array, &block)
  array.map(&block)
 end
 
-def generalized_reduce(source_array, starting_value = nil, &block)
+def generalized_reduce(source_array, starting_value = 10, &block)
  if starting_value.nil? source_array.reduce(&block) 
   else source_array.reduce(starting_value, &block)
  end
