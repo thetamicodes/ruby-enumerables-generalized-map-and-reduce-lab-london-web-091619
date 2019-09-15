f56c8067398e634872bf06760c4d1b7694f6e5e8
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 15:05:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,6 +2,6 @@ def map(array, block)
   map(array) { |element| element * element }
 end
 
-def generalized_reduce(source_array, starting_value = 10)
+def generalized_reduce(source_array, starting_value = 10, block)
   source_array.reduce(starting_value) { |sum, element| sum + element }
 end
